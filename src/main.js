ec675b7fc6ece829cef278752de4f15bacad6419
aleisabl <aleisabl.25@gmail.com>
2019-02-25 12:38:40 -0300

    ajustando css, intentando reemplazar data en cards x los 151 pokemon

--- src/main.js
+++ src/main.js
@@ -9,5 +9,24 @@ document.getElementById("btn-start").addEventListener("click", () => {
       document.getElementById("kanto-img").addEventListener("click", () => {
         document.getElementById("screen2").setAttribute('hidden', true);
         document.getElementById("screen3").removeAttribute('hidden');
-    
+
+
+
+        //sobreescribiendo data en las cards de los Pokémon
+          
+        const pokemon = POKEMON.pokemon;
+        
+        for (let i = 0; i < POKEMON.length; i++) {
+        
+          document.getElementsByClassName(card-img-top).src.innerHTML = "";
+  
+          document.getElementsByClassName(card-img-top).src.innerHTML += `${pokemon[i].img}`;
+          document.getElementsByClassName(card-title).innerHTML += `<p>${pokemon[i].name}</p>`
+
+        
+        }
+        
       })
+
+
+     
