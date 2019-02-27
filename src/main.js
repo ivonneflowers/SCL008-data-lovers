07eb0855aab3fa9e2974ad8cdf7ef47cfa18a4fb
aleisabl <aleisabl.25@gmail.com>
2019-02-26 21:12:57 -0300

    ajustando fondo pantalla 3, ajustando tarjetas, creando modal cuando haces clic en el pokemon

--- src/main.js
+++ src/main.js
@@ -1,33 +1,42 @@
 /* Manejo del DOM */
 const data = window.POKEMON.pokemon;
-console.log(data)
 const showData = (data) => {
-  data.forEach(elemento => {
-    //console.log(elemento.img)
-    // console.log(elemento.name)
-    // console.log(elemento.num)
-    document.getElementById('card').innerHTML += `<div class="card" style= "width: 9rem;">
-                                                    <div class= "card-name">
-                                                    <p> ${elemento.num}</p>
-                                                    <img class= "card-img-top" src="${elemento.img}" alt="Card image cap">
-                                                    <p> ${elemento.name}</p>
-                                                    </div>
-                                                    </div>`
-                                                    
-                                                   
+
+  data.forEach(element => {
+
+    document.getElementById('card').innerHTML += `<div data-toggle="modal" data-target="#exampleModal" class="card" style= "width: 8rem;">
+    <div class= "card-name">
+    <p> ${element.num}</p>
+    <a href="#"><img class= "card-img-top" src="${element.img}" alt="Card image cap"></a>
+    <p class="pokemon-name"> ${element.name}</p>
+    </div>
+    </div>`
+
+    
+       
   })
+
 }
+
+document.getElementById("card").addEventListener("click", () => {
+
+  document.getElementById("exampleModal").removeAttribute('hidden');
+
+})
+
 
 window.onload = showData(data)
 
 document.getElementById("btn-start").addEventListener("click", () => {
-    
+  
   document.getElementById("screen1").setAttribute('hidden', true);
   document.getElementById("screen2").removeAttribute('hidden');
 
 })
 
-      document.getElementById("kanto-img").addEventListener("click", () => {
-        document.getElementById("screen2").setAttribute('hidden', true);
-        document.getElementById("screen3").removeAttribute('hidden');
-      });
+document.getElementById("kanto-img").addEventListener("click", () => {
+
+  document.getElementById("screen2").setAttribute('hidden', true);
+  document.getElementById("screen3").removeAttribute('hidden');
+
+});
