b95ab6b55a512a50c903f13a9f345f92356b4bf8
aleisabl <aleisabl.25@gmail.com>
2019-02-27 15:15:26 -0300

    justificando bienvenida, mapas,title seleccionar zona y fondos

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const showData = (data) => {
 
   data.forEach(element => {
 
-    document.getElementById('card').innerHTML += `<div data-toggle="modal" data-target="#exampleModal" class="card" style= "width: 8rem;">
+    document.getElementById('card').innerHTML += `<div data-toggle="modal" data-target="#example-modal" class="card" style= "width: 8rem;">
     <div class= "card-name">
     <p> ${element.num}</p>
     <a href="#"><img class= "card-img-top" src="${element.img}" alt="Card image cap"></a>
@@ -14,51 +14,44 @@ const showData = (data) => {
     </div>`
 
     
+    
     document.getElementById("card").addEventListener("click", () => {
 
-      document.getElementById("exampleModal").innerHTML += `<div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <img src="${element.img}" alt="modal img">
-          <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-        <p class="pokemon-type"> Tipos: ${element.type} </p>
-        <br>
-        <p class="pokemon-weaknesses"> Debilidades: ${element.weaknesses} </p>
-        <br>
-        <p class="pokemon-candy"> Caramelo: ${element.candy} </p>
-        <p class="pokemon-egg"> Distancia de huevo: ${element.egg} </p>
-        <img id="egg-img" src= "img/egg.png" alt="egg img">
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary">Ver más</button>
-        </div>
+    
+      document.getElementById("example-modal").innerHTML += `<div class="modal-dialog" role="document">
+    
+      <div class="modal-content">    
+      <div class="modal-header">
+      <img src="${element.img}" alt="modal img">
+      <h5 class="modal-title" id="example-modal-label">${element.name}</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
       </div>
-    </div>
-  </div> `
+      <div class="modal-body">
+      <p class="pokemon-type"> Tipos: ${element.type} </p>
+      <br>
+      <p class="pokemon-weaknesses"> Debilidades: ${element.weaknesses} </p>
+      <br>
+      <p class="pokemon-candy"> Caramelo: ${element.candy} </p>
+      <p class="pokemon-egg"> Distancia de huevo: ${element.egg} </p>
+      <img id="egg-img" src= "img/egg.png" alt="egg img">
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-secondary">Ver más</button>
+      </div>
+      </div>
+      </div>  
+      </div> `
 
-       
+    })
   })
-})
 }
-
-
-
-/* document.getElementById("card").addEventListener("click", () => {
-
-  document.getElementById("exampleModal").removeAttribute('hidden');
-
-}) */
-
 
 window.onload = showData(data)
 
 document.getElementById("btn-start").addEventListener("click", () => {
-  
+
   document.getElementById("screen1").setAttribute('hidden', true);
   document.getElementById("screen2").removeAttribute('hidden');
 
