c9b1a35b22b97cf92429309635b00ee55e87e427
aleisabl <aleisabl.25@gmail.com>
2019-02-28 18:48:40 -0300

    haciendo pruebas para ordenar al data, haciendo select para odenar alfabeticamente

--- src/data.js
+++ src/data.js
@@ -8,8 +8,28 @@ const filterData = (data, condition) => {
 
 window.filterData = filterData;
 
-/* const sortData = () => {
-  let sortData = data.sort(element => {
 
+//haciendo pruebas para ordenar la data
+
+ /* const sortData = () => {
+  let sortData = data.sort(element => {
+    let pokemoncito= POKEMON.pokemon
+    for (let i = 0; i < pokemoncito.length; i++){
+      let order= [];
+      let name = [];
+      name.push(`${pokemoncito[i].name}`);
+      order = name.sort((primerPokemon,segundoPokemon)=>{
+        return primerPokemon.localeCompare(segundoPokemon);
+
+      })
+    };
   })
-} */
+} 
+
+const name = ["Maria", "Juanita", "Ale"]
+
+const nameOrden = name.sort((a,b) => {
+return a.localeCompare(b);
+});
+
+window.sortData = sortData; */
