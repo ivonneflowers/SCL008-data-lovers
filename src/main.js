2c2c63f448cd4551664c606734bcdba771823f1e
aleisabl <aleisabl.25@gmail.com>
2019-03-05 12:02:48 -0300

    arreglando funcion compute stats declarando variables para que funcione con cualquier data, ya funcionan tests de sortdata y computestats

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 const data = window.POKEMON.pokemon;
 const card = document.getElementById('card');
 
-
 /* window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -18,50 +17,6 @@ function topFunction() {
  document.documentElement.scrollTop = 0;
 } */
 
-document.getElementById('select-order').addEventListener("change", orderThis);
-function orderThis () {
- let result = window.sortData(data);
- card.innerHTML = "";
- result.forEach(element => {
-   card.innerHTML += `<div data-toggle="modal" data-target="#exampleModal${element.id}" class="card" style= "width: 8rem;">
-   <div class= "card-name">
-   <p> ${element.num}</p>
-   <a href="#"><img class= "card-img-top" src="${element.img}" alt="Card image cap"></a>
-   <p class="pokemon-name"> ${element.name}</p>
-   <div class="modal fade" id="exampleModal${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-   <div class="modal-dialog" role="document">
-   <div class="modal-content">
-     <div class="modal-header">
-     <div>
-     </div>
-     <div>
-     <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
-     <img  class = "pokemon-modal-img" src="${element.img}" alt="modal img">
-     </div>
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-         <span aria-hidden="true">&times;</span>
-       </button>
-     </div>
-     <div class="modal-body">
-
-     <p class="pokemon-type"> Tipos: ${element.type} </p>
-     <p class="pokemon-weaknesses"> Debilidades: ${element.weaknesses} </p>
-     <p class="pokemon-candy"> Caramelo: ${element.candy} </p>
-     <p class="pokemon-egg"> Distancia de huevo: ${element.egg} </p>
-     <img id="egg-img" src= "img/egg.png" alt="egg img">
-     </div>
-     <div class="modal-footer">
-
-       <button type="button" class="see-more">Cerrar</button>
-     </div>
-   </div>
-   </div>
-   </div>`
-
-
-
- })
-}
 const showData = (data) => {
 
  data.forEach(element => {
@@ -95,7 +50,6 @@ const showData = (data) => {
      <img id="egg-img" src= "img/egg.png" alt="egg img">
      </div>
      <div class="modal-footer">
-
        <button type="button" class="see-more">Cerrar</button>
      </div>
    </div>
@@ -120,8 +74,6 @@ const showData = (data) => {
        <div class="modal-dialog" role="document">
        <div class="modal-content">
          <div class="modal-header">
-         <div>
-         </div>
          <div>
          <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
          <img  class = "pokemon-modal-img" src="${element.img}" alt="modal img">
@@ -150,8 +102,58 @@ const showData = (data) => {
  })
 }
 
+document.getElementById('select-order').addEventListener("change", orderThis);
+function orderThis () {
+ let result = window.sortData(data);
+ card.innerHTML = "";
+ result.forEach(element => {
+   card.innerHTML += `<div data-toggle="modal" data-target="#exampleModal${element.id}" class="card" style= "width: 8rem;">
+   <div class= "card-name">
+   <p> ${element.num}</p>
+   <a href="#"><img class= "card-img-top" src="${element.img}" alt="Card image cap"></a>
+   <p class="pokemon-name"> ${element.name}</p>
+   <div class="modal fade" id="exampleModal${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+   <div class="modal-content">
+     <div class="modal-header">
+     <div>
+     <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
+     <img  class = "pokemon-modal-img" src="${element.img}" alt="modal img">
+     </div>
+       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
+     </div>
+     <div class="modal-body">
+
+     <p class="pokemon-type"> Tipos: ${element.type} </p>
+     <p class="pokemon-weaknesses"> Debilidades: ${element.weaknesses} </p>
+     <p class="pokemon-candy"> Caramelo: ${element.candy} </p>
+     <p class="pokemon-egg"> Distancia de huevo: ${element.egg} </p>
+     <img id="egg-img" src= "img/egg.png" alt="egg img">
+     </div>
+     <div class="modal-footer">
+
+       <button type="button" class="see-more">Cerrar</button>
+     </div>
+   </div>
+   </div>
+   </div>`
+
+
+
+ })
+}
 
 window.onload = showData(data)
+
+document.getElementById("show-dato").addEventListener("click", () => {
+
+  document.getElementById("datito").innerHTML += `¿Sabías que los pokémon tipo agua son el ${window.computeStats(data)} % de la región de Kanto?`
+ 
+ })
+
+
 document.getElementById("btn-start").addEventListener("click", () => {
 
  document.getElementById("screen1").setAttribute('hidden', true);
@@ -164,5 +166,9 @@ document.getElementById("kanto-img").addEventListener("click", () => {
 
  document.getElementById("screen2").setAttribute('hidden', true);
  document.getElementById("screen3").removeAttribute('hidden');
+
+ /* document.getElementById('show-dato').innerHTML += `¿Sabías que los pokémon tipo agua son el ${window.computeStats(data)} % de la región de Kanto?` */
+
+
 
 });
